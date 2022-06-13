147ab9b2f7c3d48b7bf2266085b1131d4e11ce16
kopever <kop.still@gmail.com>
2022-06-13 23:40:40 +0800

    gin: parameters in path

--- example.go
+++ example.go
@@ -8,14 +8,31 @@ import (
 
 func main() {
 	// gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
+	router := gin.Default()
 	// r.GET("/ping", ping)
-	r.GET("/ping", func(c *gin.Context) {
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+	router.POST("/user/:name/*action", func(c *gin.Context) {
+		b := c.FullPath() == "/user/:name/*action"
+		c.String(http.StatusOK, "%t", b)
+	})
+	router.GET("/user/groups", func(c *gin.Context) {
+		c.String(http.StatusOK, "The available groups are [...]")
+	})
+	router.Run()
 }
 
 // func ping(c *gin.Context) {

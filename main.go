e0447ea7c4f377095dc7d8aa0acfe227d39c0dcd
kopever <kop.still@gmail.com>
2022-06-27 16:29:58 +0800

    JSONP

--- main.go
+++ main.go
@@ -332,6 +332,15 @@ func main() {
 		c.SecureJSON(http.StatusOK, names)
 	})
 
+	// JSONP
+	router.GET("/JSONP", func(c *gin.Context) {
+		data := gin.H{
+			"foo": "bar",
+		}
+		c.JSONP(http.StatusOK, data)
+		// curl http://127.0.0.1:8080/JSONP?callback=x
+	})
+
 	// Redis test
 	router.POST("/redis", func(c *gin.Context) {
 		var redisKVData redisKVData

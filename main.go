cf69547ba7e1911965de8dcbf3302bea0fa11a67
kopever <kop.still@gmail.com>
2022-06-30 17:26:28 +0800

    Using BasicAuth() middleware

--- main.go
+++ main.go
@@ -462,6 +462,22 @@ func main() {
 		log.Print(example)
 	})
 
+	// Using BasicAuth() middleware
+	adminAuthorized := router.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo":    "bar",
+		"austin": "1234",
+		"lena":   "hello2",
+		"manu":   "4321",
+	}))
+	adminAuthorized.GET("/secrets", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := secrets[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+		}
+	})
+
 	// Redis test
 	router.POST("/redis", func(c *gin.Context) {
 		var redisKVData redisKVData
@@ -477,6 +493,12 @@ func main() {
 	})
 
 	router.Run()
+}
+
+var secrets = gin.H{
+	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
+	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
+	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
 }
 
 func Logger() gin.HandlerFunc {
